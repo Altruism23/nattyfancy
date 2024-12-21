5c57f2f938654bc27671ad35645da59b1c229ebb
Altruism23 <amsaldikristian@gmail.com>
2024-12-21 22:16:56 +0700

    third commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Section1 from "@/components/Section1";
 
 export default function Home() {
   return (
-    <div className="w-full h-full min-h-screen bg-hero-pattern bg-cover bg-center">
+    <div className="w-full h-full min-h-screen bg-hero-pattern bg-white bg-cover bg-center">
       <Section1 />
     </div>
   );
